b6298b25bb70c68cc262ae3dd0c79aa5681fe430
Simone Callegarin <simone.callegarin@mail.polimi.it>
2022-03-31 14:26:45 +0200

    Added the following enumerations: GamePhases, PlanificationPhases and CloudSide

---
+++ src/main/java/it/polimi/ingsw/Model/CloudSide.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.Model;
+
+/**
+ * This enumeration indicates the two side of the clouds for the two different game mode of 3 or 2/4 players
+ */
+
+public enum CloudSide {
+    SIDE_3_PLAYERS,
+    SIDE_2_AND_4_PLAYERS
+}
