bedac809d155c76f46af63f2c5a2b5ce3fe0fa07
Simone Callegarin <simone.callegarin@mail.polimi.it>
2022-04-01 00:48:50 +0200

    Added the following method and test: buildDeck and BuildingAssistantCardsDeckTest

---
+++ src/test/java/it/polimi/ingsw/Model/BuildingAssistantCardsDeckTest.java
@@ -0,0 +1,37 @@
+package it.polimi.ingsw.Model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class BuildingAssistantCardsDeckTest {
+
+    /**
+     * We are testing if the assistant cards deck is built correctly for the player
+     */
+
+    @Test
+    void buildDeck() {
+
+        /**
+         * These two following arrays represent the expected values of the assistant cards parameters,
+         * there is also used that is initialized false
+         */
+
+        int[] turnOrder = new int[] {1,2,3,4,5,6,7,8,9,10};
+        int[] mnMovement = new int[] {1,1,2,2,3,3,4,4,5,5};
+
+
+        ArrayList<AssistantCard> deckForTest = new ArrayList<AssistantCard>();
+        Player playerForTest = new Player("PlayerForTest", Squads.SQUAD1, Mages.MAGE1, deckForTest, null  );
+        playerForTest.buildDeck(Mages.MAGE1);
+
+        for(int i = 0; i < 10; i++) {
+            assertEquals(turnOrder[i], playerForTest.getMageDeck().get(i).turnOrder);
+            assertEquals(mnMovement[i], playerForTest.getMageDeck().get(i).mnMovement);
+            assertEquals(false, playerForTest.getMageDeck().get(i).used);
+        }
+    }
+}
