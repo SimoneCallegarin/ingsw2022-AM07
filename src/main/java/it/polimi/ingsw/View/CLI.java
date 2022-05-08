6b9c0676f0ddcf5fdd767b3ed489cd276770638c
budaf <buda.filippo00@gmail.com>
2022-05-08 15:02:19 +0200

    Updated CLI draw methods and added a main method to test them

--- src/main/java/it/polimi/ingsw/View/CLI.java
+++ src/main/java/it/polimi/ingsw/View/CLI.java
@@ -1,12 +1,14 @@
 package it.polimi.ingsw.View;
 
+import it.polimi.ingsw.Model.DashboardObjects.Dashboard;
+import it.polimi.ingsw.Model.Enumeration.RealmColors;
 import it.polimi.ingsw.Observer.ModelObserver;
-import it.polimi.ingsw.Observer.ViewObserver;
 import it.polimi.ingsw.Observer.ViewSubject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import java.util.HashMap;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
@@ -17,14 +19,14 @@ import java.util.concurrent.FutureTask;
  */
 
 public class CLI extends ViewSubject implements ModelObserver {
-
-    private final BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 
     /**
      * method used to launch a thread for user input reading
-     * @return the String read from the standard input
      */
-    public String readUserInput() throws ExecutionException, InterruptedException {
+    //need to make it always run not only when the method is called
+    public String readUserInput(){
+
         FutureTask<String> asyncInput=new FutureTask<>(new Callable<String>() {
             @Override
             public String call() throws Exception {
@@ -37,11 +39,13 @@ public class CLI extends ViewSubject implements ModelObserver {
         try {
              userInput = asyncInput.get();
         }
-        catch (InterruptedException e){
+        catch (InterruptedException | ExecutionException e){
             asyncInput.cancel(true);
             Thread.currentThread().interrupt();
         }
         return userInput;
+
+
     }
 
     /**
@@ -70,31 +74,73 @@ public class CLI extends ViewSubject implements ModelObserver {
      */
     public void askUsername(){
         System.out.println("Enter your username:\n");
-        try{
-            String username=readUserInput();
-            notifyObserver(obs->obs.onUsername(username));
-        } catch (ExecutionException | InterruptedException e) {
-            e.printStackTrace();
-        }
+        String username=readUserInput();
+        notifyObserver(obs->obs.onUsername(username));
     }
 
     public void askGamePreferences(){
         System.out.println("Enter the number of players desired:\n");
-        try{
-            int numPlayers=Integer.parseInt(readUserInput());
-            System.out.println("Now type the game mode preffered: Expert or Base?");
-            String choiceGamemode=readUserInput();
-            boolean gamemode=choiceGamemode.equals("Expert");
-            notifyObserver(obs->obs.onGamePreferences(numPlayers, gamemode));
-        } catch (ExecutionException | InterruptedException e) {
-            e.printStackTrace();
+        int numPlayers=Integer.parseInt(readUserInput());
+        System.out.println("Now type the game mode preferred: Expert or Base?");
+        String choiceGamemode=readUserInput();
+        boolean gamemode=choiceGamemode.equals("Expert");
+        notifyObserver(obs->obs.onGamePreferences(numPlayers, gamemode));
+    }
+
+    public static void drawDashboard(Dashboard dashboard){
+
+    }
+
+    public static StringBuilder drawDiningRoom(HashMap<RealmColors, Integer> students){
+        StringBuilder toPrint=new StringBuilder();
+        toPrint.append("DiningRoom\n");
+        for(RealmColors color: RealmColors.values()){
+            for(int i=0;i<10;i++){
+                if(i<students.get(color)){
+                    toPrint.append(CLIColors.realmColorsConverter(color)).append("O ").append(CLIColors.RESET);
+                }else{
+                    toPrint.append(CLIColors.realmColorsConverter(color)).append("X ").append(CLIColors.RESET);
+                }
+            }
+            toPrint.append("\n");
         }
-    }
-
-    public void drawBoard(){
+        return toPrint;
 
     }
 
+    public static StringBuilder drawEntrance(HashMap<RealmColors, Integer> students){
+        StringBuilder toPrint= new StringBuilder();
+        toPrint.append("Entrance\n");
+        for(RealmColors color:students.keySet()){
+            for(int i=0;i<students.get(color);i++){
+                toPrint.append(CLIColors.realmColorsConverter(color)+"O "+CLIColors.RESET);
+            }
+            toPrint.append("\n");
+        }
+        return toPrint;
+    }
 
+    //need to add the color of the Tower. The TowerColor are Grey, Black, White
+    public static StringBuilder drawTowerStorage(int numTower){
+        StringBuilder toPrint=new StringBuilder();
+        for(int i=0;i<numTower && i<8;i++){
+            toPrint.append("O ");
+            if(i%2!=0){
+                toPrint.append("\n");
+            }
+        }
+        return toPrint;
+    }
 
+    public static void main(String[] args) {
+        HashMap<RealmColors,Integer> students=new HashMap<>();
+        students.put(RealmColors.RED,2);
+        students.put(RealmColors.YELLOW,4);
+        students.put(RealmColors.PINK,3);
+        students.put(RealmColors.BLUE,1);
+        students.put(RealmColors.GREEN,0);
+        System.out.println(drawEntrance(students));
+        System.out.println(drawDiningRoom(students));
+        System.out.println(drawTowerStorage(4));
+    }
 }
