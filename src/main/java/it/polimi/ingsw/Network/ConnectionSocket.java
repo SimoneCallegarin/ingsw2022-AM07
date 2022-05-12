f76e332e01467e0d396e7cd6cc52105545961567
budaf <buda.filippo00@gmail.com>
2022-05-12 19:59:20 +0200

    Updated View Observer Architecture

--- src/main/java/it/polimi/ingsw/Network/ConnectionSocket.java
+++ src/main/java/it/polimi/ingsw/Network/ConnectionSocket.java
@@ -5,6 +5,7 @@ import it.polimi.ingsw.Network.Messages.*;
 import it.polimi.ingsw.Network.Messages.NetworkMessages.GamePreferencesMessage;
 import it.polimi.ingsw.Network.Messages.NetworkMessages.LoginMessage;
 import it.polimi.ingsw.Network.Messages.NetworkMessages.NetworkMessage;
+import it.polimi.ingsw.Network.Messages.NetworkMessages.PlayerMoveMessage;
 import it.polimi.ingsw.Observer.ViewObserver;
 
 import java.io.IOException;
@@ -12,7 +13,7 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class ConnectionSocket {
+public class ConnectionSocket implements ViewObserver {
 
     private final String host;
     private final int port;
@@ -20,17 +21,30 @@ public class ConnectionSocket {
     ClientListener cListener;
     MessageSerializer mSerializer;
     Scanner in;
+    Scanner inStream;
+
 
     public void send(NetworkMessage message, MessageType mt) {
         String messageJSON = mSerializer.serializeMessage(message, mt);
         outStream.println(messageJSON);
     }
 
-    public ConnectionSocket() {
+    public ConnectionSocket() throws IOException {
         this.host = ServerSettings.ReadHostFromJSON();
         this.port = ServerSettings.ReadPortFromJSON();
         this.mSerializer = new MessageSerializer();
         this.in = new Scanner(System.in);
+        Socket clientSocket = null;
+        try {
+            clientSocket = new Socket("localhost", ServerSettings.ReadPortFromJSON());
+            System.out.println("Connection established.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.inStream=new Scanner(clientSocket.getInputStream());
+        this.cListener = new ClientListener(clientSocket, inStream);
+        Thread thread = new Thread(this.cListener);
+        thread.start();
     }
 
     public void setup(Scanner inStream) {
@@ -69,9 +83,9 @@ public class ConnectionSocket {
         System.out.println("You joined a game!");
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
 
-        ConnectionSocket clientConnection = new ConnectionSocket();
+        /*ConnectionSocket clientConnection = new ConnectionSocket();
 
         Socket clientSocket = null;
         try {
@@ -82,11 +96,85 @@ public class ConnectionSocket {
         }
         clientConnection.outStream = new PrintWriter(clientSocket.getOutputStream(),true);
         Scanner inStream = new Scanner(clientSocket.getInputStream());
+
         clientConnection.setup(inStream);
+
         clientConnection.cListener = new ClientListener(clientSocket, inStream);
         Thread thread = new Thread(clientConnection.cListener);
         thread.start();
+         */
     }
 
+    @Override
+    public void onUsername(String username) throws Exception {
+        String messageReceived;
+        Exception IOException = new Exception("Username already Taken");
+        LoginMessage logMessage = new LoginMessage(username);
+        send(logMessage, logMessage.getMessageType());
+        messageReceived=inStream.nextLine();
+        if (messageReceived.equals(ConstantMessages.koJSON)){
+            throw IOException;
+        }
+    }
+
+    @Override
+    public void onGamePreferences(int numPlayers, Boolean gameMode) {
+        String messageReceived;
+        GamePreferencesMessage gSetMessage = new GamePreferencesMessage(numPlayers, gameMode);
+        do {//the input is controlled by the view so the only errors that can occur are network message corruption
+            send(gSetMessage, gSetMessage.getMessageType());
+            messageReceived = inStream.nextLine();
+        } while (!messageReceived.equals(ConstantMessages.okJSON));
+    }
+
+
+    @Override
+    public void onColorChoice(int color) {
+        PlayerMoveMessage playerMoveMessage=new PlayerMoveMessage(MessageType.VALUE,0,color);
+        send(playerMoveMessage,playerMoveMessage.getMessageType());
+    }
+
+    @Override
+    public void onStudentmovement_toIsle(int isleId) {
+        PlayerMoveMessage playerMoveMessage = new PlayerMoveMessage(MessageType.MOVE_STUDENT_TO_ISLE, 0, isleId);
+        send(playerMoveMessage, playerMoveMessage.getMessageType());
+    }
+
+    @Override
+    public void onStudentmovement_toDining(int dining) {
+        PlayerMoveMessage playerMoveMessage=new PlayerMoveMessage(MessageType.MOVE_STUDENT_TO_DINING,0,dining);
+        send(playerMoveMessage,playerMoveMessage.getMessageType());
+    }
+
+    @Override
+    public void onCharacterCard(int characterId) {
+        PlayerMoveMessage playerMoveMessage=new PlayerMoveMessage(MessageType.PLAY_CHARACTER_CARD,0,characterId);
+        send(playerMoveMessage,playerMoveMessage.getMessageType());
+    }
+
+    @Override
+    public void onAssistantCard(int turnOrder) {
+        PlayerMoveMessage playerMoveMessage=new PlayerMoveMessage(MessageType.PLAY_ASSISTANT_CARD,0,turnOrder);
+        send(playerMoveMessage,playerMoveMessage.getMessageType());
+    }
+
+    @Override
+    public void onAtomicEffect(int genericValue) {
+        PlayerMoveMessage playerMoveMessage=new PlayerMoveMessage(MessageType.ACTIVATE_ATOMIC_EFFECT,0,genericValue);
+        send(playerMoveMessage,playerMoveMessage.getMessageType());
+    }
+
+    @Override
+    public void onMNMovement(int idIsle) {
+        PlayerMoveMessage playerMoveMessage=new PlayerMoveMessage(MessageType.MOVE_MOTHERNATURE,0,idIsle);
+        send(playerMoveMessage,playerMoveMessage.getMessageType());
+
+    }
+
+    @Override
+    public void onCloudChoice(int idCloud) {
+        PlayerMoveMessage playerMoveMessage=new PlayerMoveMessage(MessageType.CHOOSE_CLOUD,0,idCloud);
+        send(playerMoveMessage,playerMoveMessage.getMessageType());
+    }
 }
 
--- src/main/java/it/polimi/ingsw/View/CLI.java
+++ src/main/java/it/polimi/ingsw/View/CLI.java
@@ -1,16 +1,23 @@
 package it.polimi.ingsw.View;
 
-import it.polimi.ingsw.Model.Game;
+
+
+import it.polimi.ingsw.Model.Enumeration.RealmColors;
+import it.polimi.ingsw.Network.ConnectionSocket;
+import it.polimi.ingsw.Network.Server;
 import it.polimi.ingsw.Observer.ViewSubject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * this class implements the command line interface to play trough terminal, it's observed by the connectionSocket which sends messages
- * to the server according to the CLI updates and, in addition, it observes the model to update the graphics accordingly.
+ * to the server according to the CLI updates and, in addition, it receives update messages from the virtual view to manage the interactions
+ * with the player accordingly.
  */
 
 public class CLI extends ViewSubject {
@@ -50,7 +57,14 @@ public class CLI extends ViewSubject {
         //the model receives these data through the network then after it updates, it sends the new datas trough to the Client
         //trough the VirtualView
         //we can't send the game reference trough messages so we need to pass each one of the objects to draw them
-        System.out.println(cliDrawer.drawGameTable(new Game()));
+        System.out.println(cliDrawer.printGameTable());
+        askAssistantCard();
+        askMove();
+        askMNMovement();
+        askCloud();
+        //the turn changes
+
+
 
 
     }
@@ -58,205 +72,143 @@ public class CLI extends ViewSubject {
     /**
      * method used to read the username choice by the player
      */
-    public void askUsername(){
+    public void askUsername() {
+        AtomicBoolean usernameTaken = new AtomicBoolean(true);
         System.out.println("Enter your username:\n");
-        String username=readUserInput();
-        notifyObserver(obs->obs.onUsername(username));
+        String username = readUserInput();
+        while (usernameTaken.get()) {
+                notifyObserver(obs -> {
+                    try {
+                        obs.onUsername(username);
+                        usernameTaken.set(false);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                });
+        }
     }
 
     /**
      * this method is used to ask the user the game settings he desires, which are the game mode and the number of Players
      */
-    public void askGamePreferences(){
-        System.out.println("Enter the number of players desired:\n");
-        int numPlayers=Integer.parseInt(readUserInput());
-        System.out.println("Now type the game mode preferred: Expert or Base?");
-        String choiceGamemode=readUserInput();
-        boolean gamemode=choiceGamemode.equals("Expert");
-        notifyObserver(obs->obs.onGamePreferences(numPlayers, gamemode));
+    public void askGamePreferences() {
+        int numPlayers = 3;
+        do {
+            if (numPlayers < 2 || numPlayers > 4) {
+                System.out.println("Invalid input, repeat");
+            }
+            System.out.println("Enter the number of players desired:\n");
+            numPlayers = Integer.parseInt(readUserInput());
+        } while (numPlayers < 2 || numPlayers > 4);
+        boolean gamemode;
+        String choiceGamemode;
+        do {
+            System.out.println("Now type the game mode preferred: Expert or Base?");
+            choiceGamemode = readUserInput();
+            gamemode = choiceGamemode.equals("Expert");
+        } while (choiceGamemode != "Expert" || choiceGamemode!="Base");
+
+        int finalNumPlayers = numPlayers;
+        boolean finalGamemode = gamemode;
+        notifyObserver(obs -> obs.onGamePreferences(finalNumPlayers, finalGamemode));
     }
-    /*
-    public static StringBuilder drawClouds(Cloud cloud1, Cloud cloud2){//~
-        StringBuilder toPrint=new StringBuilder();
-        toPrint.append(" ~~~~~     ~~~~~\n" +
-                       "{     }   {     }\t\n" +
-                       " ~~~~~     ~~~~~").append("\n");
-        for(RealmColors color:RealmColors.values()){
-            if(cloud1.getStudentsByColor(color)!=0){
-                StringBuilder support=new StringBuilder();
 
-                toPrint.append(CLIColors.realmColorsConverter(color));
-                toPrint.append(cloud1.getStudentsByColor(color)).append(color);
+    public void askCloud(){
+        int choice;
+        System.out.println("Select the cloud where do you want to take the students from");//to update with the available clouds
+        choice=Integer.parseInt(readUserInput());
+        notifyObserver(obs->obs.onCloudChoice(choice));
+        System.out.println(cliDrawer.printGameTable());
+    }
 
-                support.append(CLIColors.realmColorsConverter(color));
-                support.append(cloud1.getStudentsByColor(color)).append(color);
-                int offset=CLIColors.realmColorsConverter(color).toString().length();
-                for(int i=0;support.length()-offset+i<10;i++){
-                    toPrint.append(" ");
+    public void askMNMovement(){
+        int choice;
+        System.out.println("Select the isle id where you want to move Mother Nature");//to update with the available movements
+        choice=Integer.parseInt(readUserInput());
+        notifyObserver(obs->obs.onMNMovement(choice));
+        System.out.println(cliDrawer.printGameTable());
+    }
+
+
+    public void askAssistantCard(){
+        int choice;
+        System.out.println("Choose an Assistant Card to play:");//to update with the available assistant cards
+        choice=Integer.parseInt(readUserInput());
+        notifyObserver(obs->obs.onAssistantCard(choice));
+        System.out.println(cliDrawer.printGameTable());
+    }
+
+
+    public void askMove(){
+        int studentMoves=3;
+        int choice=0;
+        int color;
+        boolean characterUsed=false;
+        boolean turnFinished=false;
+
+            //continua a chiedere finchè non esaurisce le mosse per il primo turno o sbaglia l'input
+        while(!turnFinished) {
+            System.out.println("What is your next move? Write:");
+            System.out.println("-1 to move a student(max " + studentMoves + " other time/s this turn");
+            if (!characterUsed) {
+                System.out.println("-2 to play a Character Card");
+            }
+            choice = Integer.parseInt(readUserInput());
+
+            if (choice == 1) {
+                askStudentMovement();
+                studentMoves--;
+                if (studentMoves == 0) {
+                    turnFinished = true;
                 }
-            }
-            if(cloud2.getStudentsByColor(color)!=0){
-                toPrint.append(CLIColors.realmColorsConverter(color));
-                toPrint.append(cloud2.getStudentsByColor(color)).append(color);
-                toPrint.append("\n");
-            }
-        }
-        return toPrint;
-    }
-
-    public static StringBuilder drawIsles(IsleManager isleManager){
-        StringBuilder toPrint=new StringBuilder();
-
-        for(int i=0;i<isleManager.getIsles().size();i++){
-            toPrint.append("/¯¯¯¯¯¯¯\\   ");
-        }
-        toPrint.append("\n");
-        for (int i=0;i<isleManager.getIsles().size();i++){
-            toPrint.append("│     ");
-            if(isleManager.getIsle(i).getMotherNature()){
-                toPrint.append("X");
-            }else{
-                toPrint.append(" ");
-            }
-
-            if(isleManager.getIsle(i).getTowersColor()!=TowerColors.NOCOLOR){
-                toPrint.append(CLIColors.towerColorsConverter(isleManager.getIsle(i).getTowersColor()));
-                toPrint.append("T");
-                toPrint.append(CLIColors.RESET);
-            }else{
-                toPrint.append(" ");
-            }
-            toPrint.append("│   ");
-        }
-        toPrint.append("\n");
-        for (int i=0;i<isleManager.getIsles().size();i++){
-            toPrint.append("│       │   ");
-        }
-        toPrint.append("\n");
-        for (int i=0;i<isleManager.getIsles().size();i++) {
-            toPrint.append("\\_______/   ");
-        }
-        toPrint.append("\n");
-
-        for(RealmColors color:RealmColors.values()){
-            for(int i=0;i< isleManager.getIsles().size();i++){
-                StringBuilder subColor=new StringBuilder();
-
-                toPrint.append(CLIColors.realmColorsConverter(color));
-                toPrint.append(isleManager.getIsle(i).getStudentsByColor(color)).append(color);
-
-                subColor.append(CLIColors.realmColorsConverter(color)).append(isleManager.getIsle(i).getStudentsByColor(color)).append(color);
-
-                //number of students alignment
-                //offset eliminates the characters used to codify the colors from the length of the string
-                int offset=CLIColors.realmColorsConverter(color).toString().length();
-                for(int j=0;subColor.length()-offset+j<12;j++){
-                    toPrint.append(" ");
-                }
-            }
-            toPrint.append("\n");
-        }
-        toPrint.append(CLIColors.RESET);
-        return toPrint;
-    }
-
-    public static StringBuilder drawDashboard(Dashboard dashboard){
-        int numTower=dashboard.getTowerStorage().getNumberOfTowers();
-
-        StringBuilder entrance=drawEntrance(dashboard.getEntrance());
-        StringBuilder diningRoom=drawDiningRoom(dashboard.getDiningRoom());
-
-        StringBuilder toPrint=new StringBuilder();
-
-        //titles
-        String entranceTitle="Entrance";
-        String diningTitle="DiningRoom";
-        String towerTitle="TowerStorage";
-
-        toPrint.append(entranceTitle).append("    ").append(diningTitle).append("                  ").append(towerTitle).append("\n");
-        //Students cut and paste
-        for(RealmColors color:RealmColors.values()){
-            String substringEntrance= entrance.substring(entrance.indexOf(CLIColors.realmColorsConverter(color).toString()),entrance.indexOf("\n",entrance.indexOf(CLIColors.realmColorsConverter(color).toString())));
-            String substringDining= diningRoom.substring(diningRoom.indexOf(CLIColors.realmColorsConverter(color).toString()),diningRoom.indexOf("\n",diningRoom.indexOf(CLIColors.realmColorsConverter(color).toString())));
-            toPrint.append(substringEntrance);
-
-            //Entrance and DiningRoom representation alignment
-            //offset eliminates the characters used to codify the colors from the length of the string
-            int offset=CLIColors.realmColorsConverter(color).toString().length()+CLIColors.RESET.toString().length();
-            for(int i=1;substringEntrance.length()-offset+i<=10;i++){
-                toPrint.append(" ");
-            }
-            toPrint.append("||  ").append(substringDining).append("  ");
-
-            //Tower storage drawing
-            toPrint.append(CLIColors.towerColorsConverter(dashboard.getTowerStorage().getTowerColor()));
-            for(int j=0;j<2 && numTower>0;j++){
-                toPrint.append("T ");
-                numTower--;
-            }
-            toPrint.append(CLIColors.RESET).append("\n");
-
-
-
-        }
-        //tower cut and paste
-        return toPrint;
-
-
-
-    }
-
-    public static StringBuilder drawDiningRoom(DiningRoom diningRoom){
-        StringBuilder toPrint=new StringBuilder();
-        toPrint.append("DiningRoom\n");
-        for(RealmColors color: RealmColors.values()){
-            toPrint.append(CLIColors.realmColorsConverter(color));
-            for(int i=0;i<10;i++){
-                if(i<diningRoom.getStudentsByColor(color)){
-                    toPrint.append("S ");
-                }else{
-                    toPrint.append("X ");
-                }
-            }
-            toPrint.append(CLIColors.RESET);
-            toPrint.append("||");
-            toPrint.append(CLIColors.realmColorsConverter(color));
-            if(diningRoom.getProfessorByColor(color)==1){
-                toPrint.append(" P");
-            }else {
-                toPrint.append(" X");
-            }
-            toPrint.append(CLIColors.RESET);
-            toPrint.append("\n");
-        }
-        return toPrint;
-    }
-
-    public static StringBuilder drawEntrance(Entrance entrance){
-        StringBuilder toPrint= new StringBuilder();
-
-        toPrint.append("Entrance\n");
-        for(RealmColors color:RealmColors.values()) {
-            if (entrance.getStudentsByColor(color) == 0) {
-                toPrint.append(CLIColors.realmColorsConverter(color)).append(CLIColors.RESET);
-                toPrint.append("\n");
+            } else if (choice == 2 && !characterUsed) {
+                askCharacterToPlay();
             } else {
-                toPrint.append(CLIColors.realmColorsConverter(color));
-                for (int i = 0; i < entrance.getStudentsByColor(color); i++) {
-                    toPrint.append("S");
-                }
-                toPrint.append(CLIColors.RESET);
-                toPrint.append("\n");
+                System.out.println("Invalid choice, repeat");
             }
         }
-        return toPrint;
-    }
-    */
 
-    public static void main(String[] args) {
-        CLIDrawer cliDrawer=new CLIDrawer();
-        cliDrawer.printTitle();
-       // System.out.println(cliDrawer.drawGameTable());
+    }
+
+    public void askCharacterToPlay(){
+        final int choice;
+        System.out.println("Choose a CharacterCard to Play");
+        System.out.println("-1");
+        System.out.println("-2");
+        System.out.println("-3");
+        choice=Integer.parseInt(readUserInput());
+        notifyObserver(obs->obs.onCharacterCard(choice));
+        //to finish
+    }
+
+
+    public void askStudentMovement(){
+        int color;
+        int choice;
+        System.out.println("Choose a student color to move from the Entrance");
+        System.out.println("Colors available:");
+        color=(Integer.parseInt(readUserInput()));
+        notifyObserver(obs->obs.onColorChoice(color));
+
+        System.out.println("Choose where to move the student");
+        System.out.println("-1 to DiningRoom\n-2 to an Isle");
+        choice=Integer.parseInt(readUserInput());
+        if(choice==1){
+            notifyObserver(obs->obs.onStudentmovement_toDining(0));//to update with the idPlayer value
+            System.out.println(cliDrawer.printGameTable());
+        }else{
+            System.out.println("Write the id of the isle where you want to move the student");
+            choice=Integer.parseInt(readUserInput());
+            int finalChoice = choice;
+            notifyObserver(obs->obs.onStudentmovement_toIsle(finalChoice));
+            System.out.println(cliDrawer.printGameTable());
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        Server server=new Server(1234);
+        ConnectionSocket connectionSocket=new ConnectionSocket();
 
     }
 
