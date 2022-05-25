f5f1369fbcd10bd2b06661aa09da586419e2d30d
budaf <buda.filippo00@gmail.com>
2022-05-25 15:17:34 +0200

    Updated the IsleManagerPanel ti draw the isles in a cirlce

--- src/main/java/it/polimi/ingsw/View/GUI/IsleManagerPanel.java
+++ src/main/java/it/polimi/ingsw/View/GUI/IsleManagerPanel.java
@@ -8,11 +8,26 @@ public class IsleManagerPanel extends JPanel {
     public IsleManagerPanel() {
         setBackground(Color.CYAN);
         setLayout(new GridBagLayout());
-
-
-
         GridBagConstraints c=new GridBagConstraints();
         setBorder(BorderFactory.createLineBorder(Color.black));
+        JPanel isleContainerDx=new JPanel(new GridLayout(4,1));
+        JPanel isleContainerCenter=new JPanel();
+        JPanel isleContainerSx=new JPanel(new GridLayout(4,1));
+        c.gridy=0;
+        c.gridx=0;
+        add(isleContainerSx,c);
+        isleContainerSx.add(new IslePanel());
+        isleContainerSx.add(new IslePanel());
+        isleContainerSx.add(new IslePanel());
+        isleContainerSx.add(new IslePanel());
+        c.gridx=1;
+        add(isleContainerCenter,c);
+        c.gridx=2;
+        add(isleContainerDx,c);
+        isleContainerDx.add(new IslePanel());
+        isleContainerDx.add(new IslePanel());
+        isleContainerDx.add(new IslePanel());
+        isleContainerDx.add(new IslePanel());
 
     }
 
