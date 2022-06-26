5ff67eb18d369c088f5f35e2088da998e7e00d72
budaf <buda.filippo00@gmail.com>
2022-06-26 16:32:09 +0200

    Reorganized the disposition of game objects on the panel. Now each of the students color is on a separate row,instead Mother nature, the towers and the deny cards are all on the same row. Furthermore, now once reached five students on an isle, a label with their number will be displayed instead of the actual buttons. The same happens with the towers (for them the limits is 4)

--- src/main/java/it/polimi/ingsw/View/GUI/IslesPanels/IslePanel.java
+++ src/main/java/it/polimi/ingsw/View/GUI/IslesPanels/IslePanel.java
@@ -14,25 +14,60 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
+/**
+ * panel representing an isle on the game board
+ */
 public class IslePanel extends JPanel {
-
+    /**
+     * ModelStorage reference used to retrieve model information
+     */
     ModelStorage storage;
+    /**
+     * Constraints used to organize the game objects (students,towers,Mother nature, deny card) in the isle panel
+     */
     GridBagConstraints constraints;
+    /**
+     * Id of the isle represented by this panel
+     */
     int isleID;
+    /**
+     * Id of the isle image drawn by this panel
+     */
     int imageID;
+    /**
+     * Boolean used to know if Mother Nature is present on this isle
+     */
     boolean motherNature=false;
-    JPanel clickablePanel;
+    /**
+     * Deny card panel used to draw the deny card if it's present on this isle
+     */
     DenyCardPanel denyCardPanel;
-
+    /**
+     * Array list used to retrieve the pngs of the isles
+     */
     ArrayList<BufferedImage> isles;
+    /**
+     * Array list used to retrieve the pngs of the students
+     */
     ArrayList<BufferedImage> students;
+    /**
+     * Array list used to retrieve the pngs of the towers
+     */
     ArrayList<BufferedImage> towers;
 
+    /**
+     * Isle panel constructor
+     * @param storage the ModelStorage reference
+     * @param isleID the isle Id of the isle this panel represents
+     * @param isles Array list used to retrieve the pngs of the isles
+     * @param students Array list used to retrieve the pngs of the students
+     * @param towers Array list used to retrieve the pngs of the towers
+     */
     public IslePanel(ModelStorage storage,int isleID, ArrayList<BufferedImage> isles, ArrayList<BufferedImage> students, ArrayList<BufferedImage> towers) {
         this.storage=storage;
         this.isleID=isleID;
         this.imageID=isleID%3+1;
-        this.clickablePanel=new JPanel();
+
         setLayout(new GridBagLayout());
         constraints=new GridBagConstraints();
         setOpaque(false);
@@ -51,76 +86,135 @@ public class IslePanel extends JPanel {
         g.drawImage(img,0,0,getWidth(),getHeight(),null);
     }
 
+    /**
+     * this method initialize the isle according to the model state retrieved from the ModelStorage reference. It's called once in the panel constructor
+     * and everytime there's need to update the graphics on screen (but only once the isle panel has been reset by the resetIsle method).
+     */
     public void initializeIsle(){
         JPanel studentContainer=new JPanel(new GridBagLayout());
         studentContainer.setOpaque(false);
-        JPanel towerContainer=new JPanel(new GridBagLayout());
-        towerContainer.setOpaque(false);
+        JPanel tower_MN_Deny_Container=new JPanel(new GridBagLayout());
+        tower_MN_Deny_Container.setOpaque(false);
 
         GridBagConstraints studentConstraints=new GridBagConstraints();
-        GridBagConstraints towerConstraints=new GridBagConstraints();
+        GridBagConstraints tower_MN_Deny_Constraints=new GridBagConstraints();
 
         constraints.gridx=0;
         constraints.gridy=0;
 
+        tower_MN_Deny_Constraints.gridx=0;
+        tower_MN_Deny_Constraints.gridy=0;
+        TowerColors towerColor=storage.getGameTable().getIsle(isleID).getTowerColor();
+
         if(storage.getGameTable().getIsle(isleID).motherNatureIsPresent()){
-            add(new MNButton(),constraints);
-            constraints.gridy++;
+            tower_MN_Deny_Container.add(new MNButton(),tower_MN_Deny_Constraints);
+            tower_MN_Deny_Constraints.gridx++;
             motherNature=true;
         }
 
         for(int i=0;i<storage.getGameTable().getIsle(isleID).getDenyCardsNumber();i++) {
             denyCardPanel = new DenyCardPanel();
             denyCardPanel.setPreferredSize(new Dimension(25, 25));
-            add(denyCardPanel, constraints);
-            constraints.gridy++;
+            tower_MN_Deny_Container.add(denyCardPanel, tower_MN_Deny_Constraints);
+            tower_MN_Deny_Constraints.gridx++;
         }
 
-        studentConstraints.gridx=0;
-        studentConstraints.gridy=0;
-        for(RealmColors color:RealmColors.values()) {
-            for (int i = 0; i < storage.getGameTable().getIsle(isleID).getStudentsByColor(color); i++) {
-                studentContainer.add(new StudentButton(color, students),studentConstraints);
-                if(studentConstraints.gridx==3){
-                    studentConstraints.gridx=-1;
-                    studentConstraints.gridy++;
+        if(towerColor!= TowerColors.NOCOLOR) {
+            int towerNumber=storage.getGameTable().getIsle(isleID).getTowerNumber();
+            if(towerNumber>=4){
+                JPanel labelPanel=new JPanel();
+                labelPanel.setPreferredSize(new Dimension(25,25));
+                labelPanel.setOpaque(false);
+
+                JLabel numberLabel=new JLabel();
+                numberLabel.setOpaque(false);
+                labelPanel.add(numberLabel);
+                numberLabel.setText(String.valueOf(towerNumber));
+
+                TowerButton towerButton=new TowerButton(towerColor, towers);
+                tower_MN_Deny_Container.add(towerButton,tower_MN_Deny_Constraints);
+                tower_MN_Deny_Constraints.gridx++;
+                tower_MN_Deny_Container.add(labelPanel,tower_MN_Deny_Constraints);
+            }else {
+                for (int i = 0; i < towerNumber; i++) {
+                    tower_MN_Deny_Container.add(new TowerButton(towerColor, towers), tower_MN_Deny_Constraints);
+                    if (tower_MN_Deny_Constraints.gridx == 3) {
+                        tower_MN_Deny_Constraints.gridx = -1;
+                        tower_MN_Deny_Constraints.gridy++;
+                    }
+                    tower_MN_Deny_Constraints.gridx++;
                 }
-                studentConstraints.gridx++;
             }
         }
-        add(studentContainer,constraints);
+        add(tower_MN_Deny_Container,constraints);
 
         constraints.gridy++;
 
-        towerConstraints.gridx=0;
-        towerConstraints.gridy=0;
+        studentConstraints.gridx=0;
+        studentConstraints.gridy=0;
 
-        if(storage.getGameTable().getIsle(isleID).getTowerColor()!= TowerColors.NOCOLOR) {
-            for (int i = 0; i < storage.getGameTable().getIsle(isleID).getTowerNumber(); i++) {
-                towerContainer.add(new TowerButton(storage.getGameTable().getIsle(isleID).getTowerColor(), towers),towerConstraints);
-                if(towerConstraints.gridx==3){
-                    towerConstraints.gridx=-1;
-                    towerConstraints.gridy++;
+        for(RealmColors color:RealmColors.values()) {
+            int studentsNumber=storage.getGameTable().getIsle(isleID).getStudentsByColor(color);
+            if(studentsNumber>=5){
+                JPanel labelPanel=new JPanel();
+                labelPanel.setPreferredSize(new Dimension(25,25));
+                labelPanel.setOpaque(false);
+
+                JLabel numberLabel=new JLabel();
+                numberLabel.setOpaque(false);
+                labelPanel.add(numberLabel);
+                numberLabel.setText(String.valueOf(studentsNumber));
+                StudentButton studentButton=new StudentButton(color,students);
+                studentContainer.add(studentButton,studentConstraints);
+                studentConstraints.gridx++;
+                studentContainer.add(labelPanel,studentConstraints);
+            }else {
+                for (int i = 0; i < studentsNumber; i++) {
+                    StudentButton studentButton=new StudentButton(color,students);
+
+                    studentContainer.add(studentButton, studentConstraints);
+                    if (studentConstraints.gridx == 3) {
+                        studentConstraints.gridx = -1;
+                        studentConstraints.gridy++;
+                    }
+                    studentConstraints.gridx++;
                 }
-                towerConstraints.gridx++;
             }
-            add(towerContainer,constraints);
+            studentConstraints.gridx=0;
+            studentConstraints.gridy++;
         }
+
+        add(studentContainer,constraints);
+
+        constraints.gridy++;
 
         this.validate();
         this.repaint();
     }
 
+    /**
+     * Getter of the isle ID represented by this isle
+     * @return the isle ID represented by this isle
+     */
     public int getIsleID() {
         return isleID;
     }
 
+    /**
+     * This method is called everytime the isle panel is updated before the initializeIsle method in order to arrange the game objects
+     * with an empty panel
+     */
     public void resetIsle(){
         this.removeAll();
         this.validate();
         this.repaint();
     }
 
+    /**
+     * Getter of isle images which are used to size the layeredPanel in the tableScreenPanel class
+     * @param id the id of the image
+     * @return the image reference
+     */
     public BufferedImage getIslesImagesByID(int id) {
         return isles.get(id);
     }
